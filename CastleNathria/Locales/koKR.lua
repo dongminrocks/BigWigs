c147aa443dac842134ded58d58020f13670bddde
dongminrocks <39166823+dongminrocks@users.noreply.github.com>
2020-12-11 07:54:15 +0900

    Update koKR.lua
    added some translations for koKR. I didn't really get to stone generals but will update as I know what they mean.
    
    P.S. Long time no see, I was busy with work but i can help with bigwigs and littlewigs for the first and maybe the second raid of Shadowlands
    
    let me know if you guys need anything,
    thanks

--- CastleNathria/Locales/koKR.lua
+++ CastleNathria/Locales/koKR.lua
@@ -1,13 +1,13 @@
 local L = BigWigs:NewBossLocale("Shriekwing", "koKR")
 if not L then return end
 if L then
-	-- L.pickup_lantern = "%s picked up the lantern!"
-	-- L.dropped_lantern = "Lantern dropped by %s!"
+	-- L.pickup_lantern = "%s 이(가) 등불 소유중!"
+	-- L.dropped_lantern = "%s 이(가) 등불 내려놓음!"
 end
 
 L = BigWigs:NewBossLocale("Huntsman Altimor", "koKR")
 if L then
-	-- L.killed = "%s Killed"
+	-- L.killed = "%s 처치"
 end
 
 L = BigWigs:NewBossLocale("Artificer Xy'mox", "koKR")
@@ -27,20 +27,20 @@ if L then
 	-- L.custom_off_experimental_desc = "These features are |cffff0000not tested|r and could |cffff0000spam|r."
 
 	-- L.anima_tracking = "Anima Tracking |cffff0000(Experimental)|r"
-	-- L.anima_tracking_desc = "Messages and Bars to track anima levels in the containers.|n|cffaaff00Tip: You might want to disable the information box or bars, depending your preference."
+	-- L.anima_tracking_desc = "각 용기에 있는 용량을 체크하기 위한 메세지와 바.|n|cffaaff00Tip: 선호도에 따라 정보 박스나 바를 비활성화 하십시오."
 
 	-- L.custom_on_stop_timers = "Always show ability bars"
 	-- L.custom_on_stop_timers_desc = "Just for testing right now"
 
-	-- L.bottles = "Bottles"
-	-- L.sins = "Sins"
+	-- L.bottles = "병 받기"
+	-- L.sins = "고뇌"
 end
 
 L = BigWigs:NewBossLocale("The Council of Blood", "koKR")
 if L then
-	-- L.macabre_start_emote = "Take your places for the Danse Macabre!" -- [RAID_BOSS_EMOTE] Take your places for the Danse Macabre!#Dance Controller#4#false"
-	-- L.custom_on_repeating_dark_recital = "Repeating Dark Recital"
-	-- L.custom_on_repeating_dark_recital_desc = "Repeating Dark Recital say messages with icons {rt1}, {rt2} to find your partner while dancing."
+	-- L.macabre_start_emote = "죽음의 무도 자리 잡으세요!" -- [RAID_BOSS_EMOTE] Take your places for the Danse Macabre!#Dance Controller#4#false"
+	-- L.custom_on_repeating_dark_recital = "암흑의 발표회 반복적 알림"
+	-- L.custom_on_repeating_dark_recital_desc = "암흑의 발표회 대상일때 반복적으로 {rt1}, {rt2} 를 채팅에 표시함으로써 파트너 쉽게찾기."
 end
 
 L = BigWigs:NewBossLocale("Stone Legion Generals", "koKR")
